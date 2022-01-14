e429b11097292be159e8bec22daee3fcc38e3d33
Your Name <you@example.com>
2022-01-14 23:45:10 +0100

    Fix (Delete): correction de la suppression d'un article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,67 @@
 import { HtmlParser } from '@angular/compiler';
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Article } from './article/article.model';
 import { ArticleserviceService } from './articleservice.service';
 import { HttpserviceService } from './httpservice.service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'TP2reddit';
-  articles:Article[]=[];
+  articles: Article[] = [];
 
-  constructor(private service:HttpserviceService){
-
-
-  }
+  constructor(private service: HttpserviceService) {}
   ngOnInit(): void {
-
-   // this.articles = this.service.getArticles();
-   this.getArticles();
+    // this.articles = this.service.getArticles();
+    this.getArticles();
   }
-  addArticle(title : HTMLInputElement, link:HTMLInputElement){
+  addArticle(title: HTMLInputElement, link: HTMLInputElement) {
+    //  this.articles.push( new Article(title.value,link.value));
 
-  //  this.articles.push( new Article(title.value,link.value));
-  this.service.addArticle(new Article(title.value,link.value)).subscribe(() => this.getArticles());
-    title.value ='';
-    link.value ='';
+    this.service
+      .addArticle(new Article(title.value, link.value))
+      .subscribe(() => this.getArticles());
+    console.log('ajout');
+    console.log(this.articles);
+    title.value = '';
+    link.value = '';
     return false;
-
   }
 
-  UpdateArticle(article:Article){
-
+  UpdateArticle(article: Article) {
     //  this.articles.push( new Article(title.value,link.value));
     this.service.UpdateArticles(article).subscribe(() => this.getArticles());
 
-      return false;
-
-    }
-  supprimerArticle(article:Article){
+    return false;
+  }
+  supprimerArticle(article: Article) {
     // let  index: number = this.articles.indexOf(article);
-
     //    this.articles.splice(index, 1);
-    this.service.deleteArticles(article.id).subscribe( {
-      next:restArticles => this.articles = restArticles,
+    this.service.deleteArticles(article.id).subscribe({
+      next: () => this.getArticles(),
       complete: () => console.log('complete!!!'),
-      error: err => console.log(`error! ${err} msg: ${err.message}`)
+      error: (err) => console.log(`error! ${err} msg: ${err.message}`),
     });
     this.getArticles();
-  return false;
-  }
-  sortedArticle():Article[]{
-return this.articles.sort((a:Article, b:Article)=>b.votes - a.votes);
+    return false;
   }
 
+  sortedArticle(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 
   getArticles() {
-     // this.service.getArticles().subscribe( restArticles => this.articles = restArticles );
+    // this.service.getArticles().subscribe( restArticles => this.articles = restArticles );
 
-     this.service.getArticles().subscribe( {
-      next:restArticles => this.articles = restArticles,
+    this.service.getArticles().subscribe({
+      next: (restArticles) => (this.articles = restArticles),
       complete: () => console.log('complete!!!'),
-      error: err => console.log(`error! ${err} msg: ${err.message}`)
+      error: (err) => console.log(`error! ${err} msg: ${err.message}`),
     });
 
     console.log('hello!');
-    }
-
-
+  }
 }
-
-
-
--- src/app/httpservice.service.ts
+++ src/app/httpservice.service.ts
@@ -4,26 +4,25 @@ import { Observable } from 'rxjs';
 import { Article } from './article/article.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpserviceService {
+  constructor(private Client: HttpClient) {}
 
-  constructor(private Client:HttpClient) { }
-
-  getArticles():Observable<Array<Article>>{
+  getArticles(): Observable<Array<Article>> {
     return this.Client.get<Array<Article>>('http://localhost:8080/articles');
-
   }
-  deleteArticles(id:number):Observable<any>{
-    return this.Client.delete('http://localhost:8080/articles/'+ id);
+  deleteArticles(id: number): Observable<any> {
+    return this.Client.delete('http://localhost:8080/articles/' + id);
   }
-  addArticle(article:Article):Observable<any>{
-
-    return this.Client.post('http://localhost:8080/articles',article);
+  addArticle(article: Article): Observable<any> {
+    return this.Client.post('http://localhost:8080/articles', article);
   }
 
-  UpdateArticles(article:Article):Observable<any>{
-
-    return this.Client.put('http://localhost:8080/articles/'+ article.id,article);
+  UpdateArticles(article: Article): Observable<any> {
+    return this.Client.put(
+      'http://localhost:8080/articles/' + article.id,
+      article
+    );
   }
 }
