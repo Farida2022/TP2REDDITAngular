c4b736bf0ea20df6b28f54c1a662af663d176c55
Your Name <you@example.com>
2022-01-15 00:00:44 +0100

    Fix(vote): correction des fonctions voteUp et voteDown

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,46 +1,43 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { Article } from './article.model';
-import { EventEmitter } from  '@angular/core';
-
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
-@Input()
-article:Article= new Article('tmp','Link');
-@Output()
-deleteEvent= new EventEmitter<Article>();
-@Output()
-updateEvent= new EventEmitter<any>();
-updatearticle:boolean=false;
+  @Input()
+  article: Article = new Article('tmp', 'Link');
+  @Output()
+  deleteEvent = new EventEmitter<Article>();
+  @Output()
+  updateEvent = new EventEmitter<any>();
+  updatearticle: boolean = false;
   constructor() {}
 
-  voteUp():boolean{
-    this.article.voteUp();
-    return false;
-  }
-  voteDown():boolean{
-    this.article.voteDown();
+  voteUp(): boolean {
+    this.article.votes++;
+    this.updateEvent.emit(this.article);
     return false;
   }
 
-Update(){
-this.updatearticle=true;
-}
-UpdateArticle(title:HTMLInputElement,link:HTMLInputElement){
-  this.article.title=title.value;
-  this.article.link=link.value;
-this.updateEvent.emit(this.article);
-  this.updatearticle=false;
-
-}
-
-
-
-
-  ngOnInit(): void {
+  voteDown(): boolean {
+    this.article.votes--;
+    this.updateEvent.emit(this.article);
+    return false;
   }
+
+  Update() {
+    this.updatearticle = true;
+  }
+  UpdateArticle(title: HTMLInputElement, link: HTMLInputElement) {
+    this.article.title = title.value;
+    this.article.link = link.value;
+    this.updateEvent.emit(this.article);
+    this.updatearticle = false;
+  }
+
+  ngOnInit(): void {}
 }
